1b6ca07f0ce77f40a02c01e3c3f20f08c560553d
silas-metz <silas.metz02@gmail.com>
2025-12-17 02:57:57 +0100

    Update CASIN_basecalling-111.sh

--- A.1 Basecalling and Core Pipeline/CASIN_basecalling-111.sh
+++ A.1 Basecalling and Core Pipeline/CASIN_basecalling-111.sh
@@ -4,20 +4,19 @@ sleep 7h
 
 dorado basecaller \
   sup \
-  /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250721/Rawdata/d0/Rohdaten/POD5_merged/Converted.pod5 \
+  /path/to/input/casin/20250721/d0/pod5_merged/Converted.pod5 \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250721/d0/Sup/gesamt.bam
+  > /path/to/output/casin/cg_context/20250721/d0/Sup/gesamt.bam
   
 dorado basecaller \
   hac \
-  /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250721/Rawdata/d0/Rohdaten/POD5_merged/Converted.pod5 \
+  /path/to/input/casin/20250721/d0/pod5_merged/Converted.pod5 \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250721/d0/Hac/gesamt.bam
-  
+  > /path/to/output/casin/cg_context/20250721/d0/Hac/gesamt.bam
   
   
   
@@ -25,19 +24,19 @@ dorado basecaller \
   
   dorado basecaller \
   sup \
-  /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250721/Rawdata/d7_CASIN/Rohdaten/POD5_merged/Converted.pod5 \
+  /path/to/input/casin/20250721/d7_CASIN/pod5_merged/Converted.pod5 \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250721/d7_CASIN/Sup/gesamt.bam
+  > /path/to/output/casin/cg_context/20250721/d7_CASIN/Sup/gesamt.bam
 
  dorado basecaller \
   hac \
-  /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250721/Rawdata/d7_CASIN/Rohdaten/POD5_merged/Converted.pod5 \
+  /path/to/input/casin/20250721/d7_CASIN/pod5_merged/Converted.pod5 \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250721/d7_CASIN/Hac/gesamt.bam
+  > /path/to/output/casin/cg_context/20250721/d7_CASIN/Hac/gesamt.bam
   
   
   
@@ -45,21 +44,19 @@ dorado basecaller \
   
   dorado basecaller \
   sup \
-  /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250721/Rawdata/d7_DMSO/Rohdaten/POD5_merged/Converted.pod5 \
+  /path/to/input/casin/20250721/d7_DMSO/pod5_merged/Converted.pod5 \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250721/d7_DMSO/Sup/gesamt.bam
+  > /path/to/output/casin/cg_context/20250721/d7_DMSO/Sup/gesamt.bam
   
   dorado basecaller \
   hac \
-  /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250721/Rawdata/d7_DMSO/Rohdaten/POD5_merged/Converted.pod5 \
+  /path/to/input/casin/20250721/d7_DMSO/pod5_merged/Converted.pod5 \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250721/d7_DMSO/Hac/gesamt.bam
-  
-  
+  > /path/to/output/casin/cg_context/20250721/d7_DMSO/Hac/gesamt.bam
   
   
   
@@ -83,20 +80,19 @@ dorado basecaller \
 
 dorado basecaller \
   sup \
-  /home/diablo/Desktop/Age_PT_new_basecalling/neues_CASIN/Age_20250729_ExpCASIN_ASHg38_ONT_MR/ID116_d0/20250729_1319_1C_PBE12620_ad882349/POD5_merged/Converted.pod5 \
+  /path/to/input/casin/20250729/ID116_d0/pod5_merged/Converted.pod5 \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250729/d0/Sup/gesamt.bam
+  > /path/to/output/casin/cg_context/20250729/d0/Sup/gesamt.bam
   
 dorado basecaller \
   hac \
-  /home/diablo/Desktop/Age_PT_new_basecalling/neues_CASIN/Age_20250729_ExpCASIN_ASHg38_ONT_MR/ID116_d0/20250729_1319_1C_PBE12620_ad882349/POD5_merged/Converted.pod5 \
+  /path/to/input/casin/20250729/ID116_d0/pod5_merged/Converted.pod5 \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250729/d0/Hac/gesamt.bam
-  
+  > /path/to/output/casin/cg_context/20250729/d0/Hac/gesamt.bam
   
   
   
@@ -104,36 +100,35 @@ dorado basecaller \
   
   dorado basecaller \
   sup \
-  /home/diablo/Desktop/Age_PT_new_basecalling/neues_CASIN/Age_20250729_ExpCASIN_ASHg38_ONT_MR/ID116_d7_CASIN/20250729_1150_1A_PAY46566_d17fb500/POD5_merged/Converted.pod5 \
+  /path/to/input/casin/20250729/ID116_d7_CASIN/pod5_merged/Converted.pod5 \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250729/d7_CASIN/Sup/gesamt.bam
+  > /path/to/output/casin/cg_context/20250729/d7_CASIN/Sup/gesamt.bam
 
  dorado basecaller \
   hac \
-  /home/diablo/Desktop/Age_PT_new_basecalling/neues_CASIN/Age_20250729_ExpCASIN_ASHg38_ONT_MR/ID116_d7_CASIN/20250729_1150_1A_PAY46566_d17fb500/POD5_merged/Converted.pod5 \
+  /path/to/input/casin/20250729/ID116_d7_CASIN/pod5_merged/Converted.pod5 \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250729/d7_CASIN/Hac/gesamt.bam
-  
+  > /path/to/output/casin/cg_context/20250729/d7_CASIN/Hac/gesamt.bam
   
   
   
   
   dorado basecaller \
   sup \
-  /home/diablo/Desktop/Age_PT_new_basecalling/neues_CASIN/Age_20250729_ExpCASIN_ASHg38_ONT_MR/ID116_d7_DMSO/20250729_1319_1E_PBE15368_d6d97c5d/POD5_merged/Converted.pod5 \
+  /path/to/input/casin/20250729/ID116_d7_DMSO/pod5_merged/Converted.pod5 \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250729/d7_DMSO/Sup/gesamt.bam
+  > /path/to/output/casin/cg_context/20250729/d7_DMSO/Sup/gesamt.bam
   
   dorado basecaller \
   hac \
-  /home/diablo/Desktop/Age_PT_new_basecalling/neues_CASIN/Age_20250729_ExpCASIN_ASHg38_ONT_MR/ID116_d7_DMSO/20250729_1319_1E_PBE15368_d6d97c5d/POD5_merged/Converted.pod5 \
+  /path/to/input/casin/20250729/ID116_d7_DMSO/pod5_merged/Converted.pod5 \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Desktop/Age_PT_new_basecalling/Silas/CASIN/Output_CG_Kontext/20250729/d7_DMSO/Hac/gesamt.bam
+  > /path/to/output/casin/cg_context/20250729/d7_DMSO/Hac/gesamt.bam
