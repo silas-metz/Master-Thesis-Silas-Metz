2c22c941cfc8d3d013f2adde2afbbf2b31502d42
silas-metz <silas.metz02@gmail.com>
2025-12-17 02:58:27 +0100

    Update PCR1_5er_basecalling.sh

--- A.1 Basecalling and Core Pipeline/PCR1_5er_basecalling.sh
+++ A.1 Basecalling and Core Pipeline/PCR1_5er_basecalling.sh
@@ -5,49 +5,48 @@ echo "Starte jetzt..."
 
 dorado basecaller \
   sup \
-  /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/merged.pod5 \
-  --reference /home/diablo/Desktop/HomoSapienshg38CLCGenomeChr.fa \
+  /path/to/input/pcr_run01/merged.pod5 \
+  --reference /path/to/reference/genome.fa \
   --no-trim \
   --modified-bases 5mC_5hmC \
   --recursive \
-  > /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/Sup/C/gesamtsup.bam
+  > /path/to/output/pcr_run01/basecalling/sup/C/gesamtsup.bam
   
-dorado demux --kit-name SQK-NBD114-24 --no-trim --output-dir /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/Sup/C/demux /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/Sup/C/gesamtsup.bam
+dorado demux --kit-name SQK-NBD114-24 --no-trim --output-dir /path/to/output/pcr_run01/basecalling/sup/C/demux /path/to/output/pcr_run01/basecalling/sup/C/gesamtsup.bam
 
 
 dorado basecaller \
   sup \
-  /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/merged.pod5 \
-  --reference /home/diablo/Desktop/HomoSapienshg38CLCGenomeChr.fa \
+  /path/to/input/pcr_run01/merged.pod5 \
+  --reference /path/to/reference/genome.fa \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/Sup/CG/gesamtsup.bam
+  > /path/to/output/pcr_run01/basecalling/sup/CG/gesamtsup.bam
   
-dorado demux --kit-name SQK-NBD114-24 --no-trim --output-dir /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/Sup/CG/demux /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/Sup/CG/gesamtsup.bam
+dorado demux --kit-name SQK-NBD114-24 --no-trim --output-dir /path/to/output/pcr_run01/basecalling/sup/CG/demux /path/to/output/pcr_run01/basecalling/sup/CG/gesamtsup.bam
 
 
 
 dorado basecaller \
   hac \
-  /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/merged.pod5 \
-  --reference /home/diablo/Desktop/HomoSapienshg38CLCGenomeChr.fa \
+  /path/to/input/pcr_run01/merged.pod5 \
+  --reference /path/to/reference/genome.fa \
   --no-trim \
   --modified-bases 5mC_5hmC \
   --recursive \
-  > /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/Hac/C/gesamtsup.bam
+  > /path/to/output/pcr_run01/basecalling/hac/C/gesamtsup.bam
   
-dorado demux --kit-name SQK-NBD114-24 --no-trim --output-dir /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/Hac/C/demux /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/Hac/C/gesamtsup.bam
+dorado demux --kit-name SQK-NBD114-24 --no-trim --output-dir /path/to/output/pcr_run01/basecalling/hac/C/demux /path/to/output/pcr_run01/basecalling/hac/C/gesamtsup.bam
 
 
 dorado basecaller \
   hac \
-  /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/merged.pod5 \
-  --reference /home/diablo/Desktop/HomoSapienshg38CLCGenomeChr.fa \
+  /path/to/input/pcr_run01/merged.pod5 \
+  --reference /path/to/reference/genome.fa \
   --no-trim \
   --modified-bases 5mCG_5hmCG \
   --recursive \
-  > /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/Hac/CG/gesamtsup.bam
+  > /path/to/output/pcr_run01/basecalling/hac/CG/gesamtsup.bam
   
-dorado demux --kit-name SQK-NBD114-24 --no-trim --output-dir /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/Hac/CG/demux /home/diablo/Einzel_Fast5_gesamt/PCR1_5er/pod5/Hac/CG/gesamtsup.bam
-
+dorado demux --kit-name SQK-NBD114-24 --no-trim --output-dir /path/to/output/pcr_run01/basecalling/hac/CG/demux /path/to/output/pcr_run01/basecalling/hac/CG/gesamtsup.bam
