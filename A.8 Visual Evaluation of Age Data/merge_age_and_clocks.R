ce2bb19532a25cc4d437444e5b23b8e7a5eefd79
silas-metz <silas.metz02@gmail.com>
2025-12-17 03:23:25 +0100

    Update merge_age_and_clocks.R

--- A.8 Visual Evaluation of Age Data/merge_age_and_clocks.R
+++ A.8 Visual Evaluation of Age Data/merge_age_and_clocks.R
@@ -6,29 +6,12 @@ suppressPackageStartupMessages({
 })
 
 
-weidner_age_file <- "/home/drk/Masterarbeit/CASIN/Output_CG_Kontext/Hac/20250729/00_Results/Filtered_by_Weidner/noH/combined_strands/with_hg19_cpgid/weidner_age_results.csv"
+weidner_age_file <- "/path/to/input/CASIN/Output_CG_Kontext/Hac/20250729/00_Results/Filtered_by_Weidner/noH/combined_strands/with_hg19_cpgid/weidner_age_results.csv"
 
-hor_han_file     <- "/home/drk/Masterarbeit/CASIN/20250729/DNAmAge_Horvath_Hannum_from_ONT.csv"
+hor_han_file     <- "/path/to/input/CASIN/20250729/DNAmAge_Horvath_Hannum_from_ONT.csv"
 
-out_merged       <- "/home/drk/Masterarbeit/CASIN/20250729/Age_all_clocks_merged.csv"
+out_merged       <- "/path/to/output/CASIN/20250729/Age_all_clocks_merged.csv"
 
-# Werte aus Altersliste:
-# Age01 23,669
-# Age02 27,244
-# Age03 27,54
-# Age04 28,567
-# Age05 30,932
-# Age06 21,265
-# Age07 27,814
-# Age08 18,598
-# PT06  72,544
-# PT09  71,926
-# PT10  78,003
-# PT17  74,402
-# PT18  72,298
-# PT21  72,022
-# PT30  79,658
-# PT36  69,615
 
 age_tbl <- tribble(
   ~id_short, ~real_age,
@@ -52,6 +35,7 @@ age_tbl <- tribble(
   "d7_CASIN", 34.500,
   "d7_DMSO", 34.500
 )
+
 
 cat("Alters-Tabelle (hardcoded):\n")
 print(age_tbl)
